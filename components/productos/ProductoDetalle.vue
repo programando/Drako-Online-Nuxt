bacc944888cab1678142771049e58f901ee3ef09
Luis Morales <luisjose.morales2@gmail.com>
2022-02-11 11:20:19 -0400

    agregando card productos

--- components/productos/ProductoDetalle.vue
+++ components/productos/ProductoDetalle.vue
@@ -1,14 +1,16 @@
 <template>
-  <div class="">
-    <img src="productos/2.jpg" alt="">
-    <p class="text-center">Kit aceite para motor</p>
-    <p class="font-semibold text-center">600.000$</p>
-  </div>
+
+  
 </template>
 
 <script>
 export default {
-  name: "ProductoDetalle"
+  name: "ProductoDetalle",
+  data() {
+    return {
+     
+    }
+  },
 }
 </script>
 
--- pages/productos.vue
+++ pages/productos.vue
@@ -2,20 +2,21 @@
   <div>
     <Header />
     <div class="grid md:grid-cols-3 lg:grid-cols-10">
-      <div class="px-8 py-10 lg:col-span-2">
+      <div class="flex justify-center px-8 py-10 lg:col-span-2">
         <FiltroProducto />
       </div>
 
-      <div class="grid grid-cols-1 py-10 md:col-span-2 lg:col-span-8 md:grid-cols-3 lg:grid-cols-6">
-        
-          <ProductoDetalle />
-          <ProductoDetalle />
-          <ProductoDetalle />
-          <ProductoDetalle />
-          <ProductoDetalle />
-          <ProductoDetalle />
-          <ProductoDetalle />
-        
+      <div
+        class="grid grid-cols-1 py-10 sm:grid-cols-2 md:col-span-3 lg:col-span-8 lg:grid-cols-4 2xl:grid-cols-5"
+      >
+        <div class="" v-for="producto in productos" :key="producto.id">
+          <div class="flex justify-center">
+            <img class="" src="/productos/1.jpg" alt="" />
+          </div>
+
+          <p class="text-center">{{ producto.name }}</p>
+          <p class="font-semibold text-center">{{ producto.price }}</p>
+        </div>
       </div>
     </div>
   </div>
@@ -24,14 +25,55 @@
 <script>
 import Header from "../components/Header.vue";
 import FiltroProducto from "../components/productos/FiltroProducto.vue";
-import ProductoDetalle from "../components/productos/ProductoDetalle.vue";
 
 export default {
   name: "Productos",
   components: {
     Header,
     FiltroProducto,
-    ProductoDetalle,
+  },
+
+  data() {
+    return {
+      productos: [
+        {
+          id: 1,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+        {
+          id: 2,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+        {
+          id: 3,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+        {
+          id: 4,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+        {
+          id: 5,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+        {
+          id: 6,
+          name: "Kit aceite para motor",
+          price: 600.0,
+          img: "https://placebear.com/200/300",
+        },
+      ],
+    };
   },
 };
 </script>
