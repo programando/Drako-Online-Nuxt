708d6651530a2372f399c1c577cc8d1641e5ce71
Luis Morales <luisjose.morales2@gmail.com>
2022-04-02 12:27:03 -0400

    modificando vista productos

--- pages/productos.vue
+++ pages/productos.vue
@@ -1,18 +1,29 @@
 <template>
   <div>
     <Header />
-    <div class="grid md:grid-cols-3 lg:grid-cols-10">
-      <div class="flex justify-center px-8 py-10 lg:col-span-3 2xl:col-span-2">
-        <FiltroProducto @getGruposSeleccionados="getGruposSeleccionados"></FiltroProducto>
-      </div>
-
-      <div class="flex py-4 mt-2 border-2">
+    <div class="fixed z-50 flex justify-center bg-white left">
+      <div class="flex py-4 mt-2 border-2 ">
         <img class="z-20 h-5 mt-2 -mr-7" src="/search.svg" alt="busqueda" />
-        <input v-model="textBuscarProducto"  @keypress.13="iniciarBusquedaProductos"
-              class="w-40 px-10 lg:w-80 xl:w-96 focus:outline-none border-r-1" type="text" placeholder="Buscar producto" />
+        <input
+          v-model="textBuscarProducto"
+          @keypress.13="iniciarBusquedaProductos"
+          class="w-40 px-10 lg:w-80 xl:w-96 focus:outline-none border-r-1"
+          type="text"
+          placeholder="Buscar producto"
+        />
+      </div>
+    </div>
+
+    <div class="grid mt-10 md:grid-cols-3 lg:grid-cols-10">
+      <div class="flex justify-center px-8 py-10 lg:col-span-3 2xl:col-span-2">
+        <FiltroProducto
+          @getGruposSeleccionados="getGruposSeleccionados"
+        ></FiltroProducto>
       </div>
 
-      <div class="grid justify-center grid-cols-1 py-10 sm:grid-cols-2 md:col-span-3 lg:col-span-7 2xl:col-span-8 lg:grid-cols-3 2xl:grid-cols-5 lg:mt-4 xl:mr-4" >
+      <div
+        class="grid justify-center min-h-screen grid-cols-1 py-10 sm:grid-cols-2 md:col-span-3 lg:col-span-7 2xl:col-span-8 lg:grid-cols-3 2xl:grid-cols-5 lg:mt-4 xl:mr-4"
+      >
         <div
           class="p-4 mx-auto mb-24 border border-white cursor-pointer w-72 hover:border hover:shadow-xl"
           v-for="(producto, index) in productos"
@@ -40,29 +51,37 @@
           </p>
           <div class="h-12">
             <p
-              class="mt-4 font-semibold text-right mr-2"
+              class="mt-4 mr-2 font-semibold text-right"
               :class="{ 'line-through text-gray-500': producto.precio_oferta }"
             >
-              {{ producto.precio_base_format  }}
+              {{ producto.precio_base_format }}
             </p>
-            <p v-if="producto.precio_oferta" class="font-semibold text-right mr-2">
-              {{ producto.precio_oferta_format  }}
+            <p
+              v-if="producto.precio_oferta"
+              class="mr-2 font-semibold text-right"
+            >
+              {{ producto.precio_oferta_format }}
             </p>
           </div>
 
           <div class="flex items-center space-x-4">
             <div class="flex items-center mt-2">
-              <button class="px-2 py-1 border" @click="disminuirCantidad(index)" >
+              <button
+                class="px-2 py-1 border"
+                @click="disminuirCantidad(index)"
+              >
                 -
               </button>
-              <p class="px-2 py-1 border" disabled="disabled">{{ producto.cantidad }}</p>
+              <p class="px-2 py-1 border" disabled="disabled">
+                {{ producto.cantidad }}
+              </p>
               <button class="px-2 py-1 border" @click="aumentarCantidad(index)">
                 +
               </button>
             </div>
             <div class="">
               <button
-                class="px-4 py-1 mt-2 border rounded-lg hover:bg-azul hover:text-white "
+                class="px-4 py-1 mt-2 border rounded-lg hover:bg-azul hover:text-white"
               >
                 Agregar al carrito
               </button>
@@ -76,11 +95,10 @@
 </template>
 
 <script>
-import Header         from    "@/components/Header.vue";
-import Footer         from    "@/components/comunes/Footer.vue";
-import FiltroProducto from    "@/components/productos/FiltroProducto.vue";
-import Productos      from    "@/models/Productos";
-
+import Header from "@/components/Header.vue";
+import Footer from "@/components/comunes/Footer.vue";
+import FiltroProducto from "@/components/productos/FiltroProducto.vue";
+import Productos from "@/models/Productos";
 
 export default {
   name: "Productos",
@@ -94,54 +112,57 @@ export default {
     return {
       productos: [],
       cantidad: 0,
-      formData:{  
-            grupos: [],
-           textoBusqueda:''
-            },
-      textBuscarProducto:''
+      formData: {
+        grupos: [],
+        textoBusqueda: "",
+      },
+      textBuscarProducto: "",
     };
   },
 
   mounted() {
-      this.getProductosAll();
+    this.getProductosAll();
   },
 
   methods: {
-          iniciarBusquedaProductos() {
-              this.formData.textoBusqueda = this.textBuscarProducto
-              Productos.busqueda (this.formData )
-              .then( response => {
-                  this.productos = response.data.data;
-              });
-          },
+    iniciarBusquedaProductos() {
+      this.formData.textoBusqueda = this.textBuscarProducto;
+      Productos.busqueda(this.formData).then((response) => {
+        this.productos = response.data.data;
+      });
+    },
 
-          aumentarCantidad(index) {
-            this.productos[index].cantidad++ ;
-          },
+    aumentarCantidad(index) {
+      this.productos[index].cantidad++;
+    },
 
-          disminuirCantidad(index) {
-            if (this.productos[index].cantidad >0){
-                this.productos[index].cantidad--  ;
-            }
-          },
+    disminuirCantidad(index) {
+      if (this.productos[index].cantidad > 0) {
+        this.productos[index].cantidad--;
+      }
+    },
 
-          getGruposSeleccionados ( idsGrupos ) {
-            this.formData.grupos = idsGrupos;
-            if  ( idsGrupos.length ==0 ) {
-              this.getProductosAll();
-              return ;
-            }
-            Productos.porGrupos ( this.formData )
-              .then ( response => {
-                  this.productos = response.data.data;
-              });
-          },
+    getGruposSeleccionados(idsGrupos) {
+      this.formData.grupos = idsGrupos;
+      if (idsGrupos.length == 0) {
+        this.getProductosAll();
+        return;
+      }
+      Productos.porGrupos(this.formData).then((response) => {
+        this.productos = response.data.data;
+      });
+    },
 
-          getProductosAll () {
-              Productos.listaGeneral().then((response) => {
-                this.productos = response.data.data;
-              });
-          }
+    getProductosAll() {
+      Productos.listaGeneral().then((response) => {
+        this.productos = response.data.data;
+      });
+    },
   },
 };
 </script>
+<style scoped>
+.left {
+  left: 33%;
+}
+</style>
