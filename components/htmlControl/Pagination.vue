f3ce2553ffd90d39676606e604c5029e29b2c19f
Luis Morales <luisjose.morales2@gmail.com>
2022-04-08 10:08:43 -0400

    modificando pagination

--- components/htmlControl/Pagination.vue
+++ components/htmlControl/Pagination.vue
@@ -1,14 +1,14 @@
 <template>
-    <div>
+    <div class="mb-4">
         <ul class="flex justify-center mt-3">
             <!-- <nuxt-link :to="link.url ? link.url : '#'" v-for="link in links" :key="link.label"   -->
             <button v-for="(link,index) in links" :key="index"  
-                class="mx-1 px-3 py-2 bg-gray-200 hover:bg-purple-500 hover:text-gray-200 rounded-lg cursor-pointer"  
-                v-bind:class="{ 'bg-purple-500': link.active, 'text-gray-200': link.active, 'text-purple-400' :!link.active}">
+                class="px-3 py-2 mx-1 bg-gray-200 rounded-lg cursor-pointer xl:mx-2 hover:bg-rojo hover:text-gray-200"  
+                v-bind:class="{ 'bg-rojo': link.active, 'text-gray-200': link.active, 'text-rojo' :!link.active}">
                 <span    class="font-bold" @click='getProductosFromUrl(link.url)'>{{getLabel(link.label)}}</span >
             </button>
         </ul>
-        <div v-if="currentRecord" class="flex justify-center">
+        <div v-if="currentRecord" class="flex justify-center mt-1">
             <span class="text-gray-700">Mostrando <span class="font-bold">{{currentRecord}}</span> de <span class="font-bold">{{totalRecords}}</span> registros.</span>
         </div>
     </div>
